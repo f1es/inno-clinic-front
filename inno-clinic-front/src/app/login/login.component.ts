3faf0d361b06d673d512ff1f9b60c3fce254d2ac
fles <anotherfles@gmail.com>
2025-03-05 17:37:01 +0300

    Install bootstrap, add login form

--- inno-clinic-front/src/app/login/login.component.ts
+++ inno-clinic-front/src/app/login/login.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = "";
+  password = "";
+  errorMessage = "";
 
+  login(){
+    if (this.username.trim().length === 0){
+      this.errorMessage = "Username is required"
+    }
+
+    if (this.password.trim().length === 0){
+      this.errorMessage = "Password is required"
+    }
+  }
 }
