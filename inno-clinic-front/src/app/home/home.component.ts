95c05b19aee801844d801effd903e990bfde01e7
fles <anotherfles@gmail.com>
2025-03-06 18:22:14 +0300

    Add basic part of home page

--- inno-clinic-front/src/app/home/home.component.ts
+++ inno-clinic-front/src/app/home/home.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  offices: any;
+  
+  constructor(private http: HttpClient) {
+    this.getOfficesAssets();
+   }
+  
+  getOfficesAssets(){
+    this.http.get("http://localhost:4200/assets/data/offices.json").subscribe((offices) => {
+      this.offices = offices;
+      console.log(this.offices);
+    })
+  }
 }
--- inno-clinic-front/src/app/login/login.component.ts
+++ inno-clinic-front/src/app/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -11,13 +14,24 @@ export class LoginComponent {
   password = "";
   errorMessage = "";
 
+  constructor(private auth: AuthService, private router: Router) { }
+
   login(){
     if (this.username.trim().length === 0){
       this.errorMessage = "Username is required"
     }
-
-    if (this.password.trim().length === 0){
+    else if (this.password.trim().length === 0){
       this.errorMessage = "Password is required"
+    }
+    else{
+      this.errorMessage = "";
+      let response = this.auth.login(this.username, this.password);
+      if (response === 200){
+        this.router.navigate(['home']);
+      }
+      else if (response === 401){
+        this.errorMessage = "Invalid credentials"
+      }
     }
   }
 }
