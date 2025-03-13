b3c0e770be60739d7eebb16455520cff09a39214
fles <anotherfles@gmail.com>
2025-03-13 15:37:00 +0300

    New image in slider

--- inno-clinic-front/src/app/login/login.component.ts
+++ inno-clinic-front/src/app/login/login.component.ts
@@ -14,9 +14,10 @@ export class LoginComponent {
   public username = "";
   public password = "";
   public errorMessage = "";
-  public slides: any[] = [
+  public slides: any[] = [  
     {src: 'https://png.klev.club/uploads/posts/2024-05/png-klev-club-cnuw-p-durka-png-15.png', alt: ''},
-    {src: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9cbba6e5-cffe-4537-a24a-305b77e34fb7/dfys10c-e03d2257-c6be-440f-b3b1-b000ef14e3d2.gif/v1/fill/w_498,h_498/maxwell_gif_by_lonelycoconut_dfys10c-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDk4IiwicGF0aCI6IlwvZlwvOWNiYmE2ZTUtY2ZmZS00NTM3LWEyNGEtMzA1Yjc3ZTM0ZmI3XC9kZnlzMTBjLWUwM2QyMjU3LWM2YmUtNDQwZi1iM2IxLWIwMDBlZjE0ZTNkMi5naWYiLCJ3aWR0aCI6Ijw9NDk4In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Sh-_Bbe5cXqEf4tKdzBkr4KlTTGfth66VC_z17UzJaM', alt: ''}
+    {src: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9cbba6e5-cffe-4537-a24a-305b77e34fb7/dfys10c-e03d2257-c6be-440f-b3b1-b000ef14e3d2.gif/v1/fill/w_498,h_498/maxwell_gif_by_lonelycoconut_dfys10c-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDk4IiwicGF0aCI6IlwvZlwvOWNiYmE2ZTUtY2ZmZS00NTM3LWEyNGEtMzA1Yjc3ZTM0ZmI3XC9kZnlzMTBjLWUwM2QyMjU3LWM2YmUtNDQwZi1iM2IxLWIwMDBlZjE0ZTNkMi5naWYiLCJ3aWR0aCI6Ijw9NDk4In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Sh-_Bbe5cXqEf4tKdzBkr4KlTTGfth66VC_z17UzJaM', alt: ''},
+    {src: 'coffin.png', alt: ''}
   ];
 
   constructor(private auth: AuthService, private router: Router) { }
