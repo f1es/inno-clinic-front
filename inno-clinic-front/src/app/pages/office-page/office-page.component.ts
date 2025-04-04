4b37f0285e8da6fb6d9e7a92297048157fcf4d5c
fles <anotherfles@gmail.com>
2025-04-04 13:55:47 +0300

    Fix error with null office

--- inno-clinic-front/src/app/pages/office-page/office-page.component.ts
+++ inno-clinic-front/src/app/pages/office-page/office-page.component.ts
@@ -12,6 +12,7 @@ import { DoctorModalComponent } from '../../modals/doctor-modal/doctor-modal.com
 import { MatDialog } from '@angular/material/dialog';
 import { ServiceModalComponent } from '../../modals/service-modal/service-modal.component';
 import { Role } from '../../services/role-service/role.enum';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-office-page',
@@ -23,7 +24,9 @@ export class OfficePageComponent {
 
   public doctors: any;
   public services: any;
-  public office: any;
+  public office: any = {
+    address: ""
+  };
   public id: string = '';
 
   constructor(
@@ -43,8 +46,8 @@ export class OfficePageComponent {
   }
 
   public getOffice(id: string) {
-    this.officeSerivce.getById(id).subscribe((office: any) => {
-      this.office = office;
+    this.officeSerivce.getById(id).subscribe((response: HttpResponse<any>) => {
+      this.office = response.body;
     });
   }
 
